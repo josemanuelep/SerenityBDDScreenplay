b53985c54a5d51acb92ac93f4da2dacdefd3d97e
josemanuelep <jecheverrip@choucairtesting.com>
2020-01-03 11:31:19 -0500

    Task finish. Going to test

--- src/main/java/co/com/choucair/certification/myFirstScreenPlay/tasks/SearchCourse.java
+++ src/main/java/co/com/choucair/certification/myFirstScreenPlay/tasks/SearchCourse.java
@@ -1,9 +1,12 @@
 package co.com.choucair.certification.myFirstScreenPlay.tasks;
 
+import co.com.choucair.certification.myFirstScreenPlay.userinterface.SearchCoursePage;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
 
 public class SearchCourse implements Task {
     private String course;
@@ -17,6 +20,9 @@ public class SearchCourse implements Task {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
-
+    actor.attemptsTo(Click.on(SearchCoursePage.BTN_UNIVERSIDAD_CH),
+            Enter.theValue(course).into(SearchCoursePage.INPUT_SEARCH_BOX),
+            Click.on(SearchCoursePage.BTN_GO_SEARCH),
+            Click.on(SearchCoursePage.COURSE_MAIN_TITLE));
     }
 }
--- src/main/java/co/com/choucair/certification/myFirstScreenPlay/userinterface/SearchCoursePage.java
+++ src/main/java/co/com/choucair/certification/myFirstScreenPlay/userinterface/SearchCoursePage.java
@@ -5,9 +5,9 @@ import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
 public class SearchCoursePage extends PageObject {
-    private static final Target BTN_UNIVERSIDAD_CH = Target.the("Click to go to the searh panel").located(By.xpath("(//STRONG)[1]"));
-    private static final Target INPUT_SEARCH_BOX = Target.the("Input to type the course to search").located(By.id("coursesearchbox"));
-    private static final Target BTN_GO_SEARCH = Target.the("Click to searh").located(By.xpath("//BUTTON[@class='btn btn-secondary'][text()='Ir']"));
-    private static final Target COURSE_MAIN_TITLE = Target.the("Visible main text if the course exists").located(By.xpath("(//H4[text()='Recursos Automatización Bancolombia'])[1]"));
+    public static final Target BTN_UNIVERSIDAD_CH = Target.the("Click to go to the searh panel").located(By.xpath("(//STRONG)[1]"));
+    public static final Target INPUT_SEARCH_BOX = Target.the("Input to type the course to search").located(By.id("coursesearchbox"));
+    public static final Target BTN_GO_SEARCH = Target.the("Click to searh").located(By.xpath("//BUTTON[@class='btn btn-secondary'][text()='Ir']"));
+    public static final Target COURSE_MAIN_TITLE = Target.the("Visible main text if the course exists").located(By.xpath("(//H4[text()='Recursos Automatización Bancolombia'])[1]"));
 
 }
